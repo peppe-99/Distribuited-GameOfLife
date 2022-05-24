146167173b7dd449422efae6eb32c9d413c2a369
peppe-99 <g.cardaropoli99@gmail.com>
2022-05-24 10:46:28 +0200

    shell script for strong scalability test

---
+++ scripts/strong_scalability.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Delete oldest performance file
+rm -rf results/strong_scalability.csv
+
+# Compile the program
+mpicc distribuited-game-of-life.c -o distribuited-game-of-life
+
+# Add collumns names to CSV file
+echo "TIME,PROCESSES,ROWS,COLS,GENERATIONS" >> results/strong_scalability.csv
+
+# Exec the program with differents num of process and matrix size
+size=12800
+for ((p=1; p<=8; p=p+1))
+do
+    echo "Start program with $p process and ($size,$size) matrix"
+    mpirun --allow-run-as-root -np $p distribuited-game-of-life $size $size 100 >> results/strong_scalability.csv
+done
+
+# Show performance
+cat results/strong_scalability.csv
